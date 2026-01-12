ad36484fbf414119e551abefa4ea666d24693021
sunilsubedi <subedisunil495@gmail.com>
2026-01-12 17:05:01 +1100

    feat: added csv files to dataframes via pandas

--- question-2/question_2.py
+++ question-2/question_2.py
@@ -3,3 +3,13 @@ import glob
 import os
 
 TEMPERATURE_DATA_FOLDER = "temperatures"
+
+all_csv_files = glob.glob(os.path.join(TEMPERATURE_DATA_FOLDER, "*.csv"))
+
+temperature_dataframes = []
+
+for temperature_csv_file in all_csv_files:
+    dataframe = pandas.read_csv(temperature_csv_file)
+    temperature_dataframes.append(dataframe)
+
+temperature_data_single_frame = pandas.concat(temperature_dataframes, ignore_index=True)
